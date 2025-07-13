81ff9fcd46f3e542fa16073c9083f5ba4fcd7ead
Fredrik Berglund <mr@blynt.com>
2025-07-14 01:18:16 +0200

    feat: Added Inter font.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,11 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
 }
