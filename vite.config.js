5cbcff87e8df2d42aa5344b37bff3d4ed22b92fe
Fredrik Berglund <mr@blynt.com>
2025-08-02 23:45:59 +0200

    fix: Updated URLs for open graph card

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,19 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+	plugins: [
+		preact(),
+		{
+			name: 'html-transform',
+			transformIndexHtml: {
+				order: 'pre',
+				handler(html, ctx) {
+					return html.replace(/%(\w+)%/g, (match, name) => {
+						const value = process.env[name];
+						return value !== undefined ? value : match;
+					});
+				}
+			}
+		}
+	],
 });
